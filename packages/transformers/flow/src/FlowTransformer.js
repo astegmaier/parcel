5962f1d87160c20893d9bd95f28edae7808d661d
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-04-11 12:36:20 +0200

    flow-remove-types

---
+++ packages/transformers/flow/src/FlowTransformer.js
@@ -0,0 +1,54 @@
+// @flow
+import type {PackageJSON} from '@parcel/types';
+import {Transformer} from '@parcel/plugin';
+
+export default (new Transformer({
+  async loadConfig({config, options}) {
+    if (!config.isSource) {
+      return false;
+    }
+
+    // Only run flow if `flow-bin` is listed as a dependency in the root package.json
+    let pkg: ?PackageJSON = (
+      await config.getConfigFrom(options.projectRoot + '/index', [
+        'package.json',
+      ])
+    )?.contents;
+
+    let shouldStripFlow =
+      pkg?.dependencies?.['flow-bin'] != null ||
+      pkg?.devDependencies?.['flow-bin'] != null;
+
+    if (shouldStripFlow) {
+      config.addDevDependency({
+        specifier: 'flow-remove-types',
+        resolveFrom: options.projectRoot + '/index',
+      });
+    }
+    return shouldStripFlow;
+  },
+
+  async transform({asset, config, options}) {
+    if (!config) {
+      return [asset];
+    }
+
+    let [code, flowRemoveTypes] = await Promise.all([
+      asset.getCode(),
+      options.packageManager.require(
+        'flow-remove-types',
+        options.projectRoot + '/index',
+        {
+          shouldAutoInstall: options.shouldAutoInstall,
+          saveDev: true,
+        },
+      ),
+    ]);
+
+    // This replaces removed code sections with spaces, so all source positions
+    // remain valid and no sourcemap is needed.
+    asset.setCode(flowRemoveTypes(code).toString());
+
+    return [asset];
+  },
+}): Transformer);
