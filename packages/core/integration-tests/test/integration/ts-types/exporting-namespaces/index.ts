041dedec2aa0cf28831b36a35e507600b1b72112
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2022-07-12 10:39:22 -0400

    Fix typo in unit test.

--- packages/core/integration-tests/test/integration/ts-types/exporting-namespaces/index.ts
+++ packages/core/integration-tests/test/integration/ts-types/exporting-namespaces/index.ts
@@ -1,6 +1,6 @@
 export * as StuffNamespaceRoot from './stuff';
 
-export { StuffNamespaceReexport } from "./re-exporter"
+export { StuffNamespaceReexported } from "./re-exporter"
 import { StuffNamespaceInternal } from "./internal-exporter"
 
 export const testInstance = new StuffNamespaceInternal.TestClass();
