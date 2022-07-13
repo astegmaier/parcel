9c21779c03de6925680f9579b56606a2e5d9c4a9
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2022-07-13 16:19:17 -0400

    Tweak indenting in expected output of unit test.

--- packages/core/integration-tests/test/integration/ts-types/exporting-namespaces/expected.d.ts
+++ packages/core/integration-tests/test/integration/ts-types/exporting-namespaces/expected.d.ts
@@ -1,15 +1,15 @@
 export namespace StuffNamespaceRoot {
-  export interface TestInterface {
-      foo: number;
-      bar: boolean;
-  }
-  export const testNumber: number;
-  export const testString: string;
-  export class TestClass {
-      a: string;
-      b: number;
-      constructor();
-  }
+    export interface TestInterface {
+        foo: number;
+        bar: boolean;
+    }
+    export const testNumber: number;
+    export const testString: string;
+    export class TestClass {
+        a: string;
+        b: number;
+        constructor();
+    }
 }
 export { StuffNamespace as StuffNamespaceReexportRenamed };
 export const testInstance: StuffNamespaceRoot.TestClass;
