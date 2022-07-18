0b26f72d72ce6f97ff2d4480231718fe58031e9f
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2022-07-18 17:11:57 -0400

    Test multiple re-export statements in a single line.

--- packages/core/integration-tests/test/integration/ts-types/exporting-namespaces/index.ts
+++ packages/core/integration-tests/test/integration/ts-types/exporting-namespaces/index.ts
@@ -1,7 +1,6 @@
 export * as StuffNamespaceRoot from './stuff';
 
-export { StuffNamespaceReexported as StuffNamespaceReexportedRenamed } from "./re-exporter"
-export { Stuff2NamespaceReexported as Stuff2NamespaceReexportedRenamed } from "./re-exporter"
+export { StuffNamespaceReexported as StuffNamespaceReexportedRenamed, Stuff2NamespaceReexported as Stuff2NamespaceReexportedRenamed } from "./re-exporter"
 import { StuffNamespaceInternal as StuffNamespaceInternalRenamed } from "./internal-exporter"
 
 export const testInstance = new StuffNamespaceInternalRenamed.TestClass();
